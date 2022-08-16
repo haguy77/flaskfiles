bf9c4d658db93a17eb1fbddc46251407040391f9
haguy77 <idelchikhaguy@gmail.com>
2022-08-16 16:45:19 +0300

    minor changes and adding more explanation

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
+# interface for uploading files
 @app.route('/upload')
 def upload_file():
     print(request.url_root)
     return render_template('upload.html')
 
 
+# endpoint that actually uploads the files (the interface send a request to this endpoint when the button is pushed)
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader():
     if request.method == 'POST':
+        # in the request you send a file and the key for it called "file"
         f = request.files['file']
+        # not actually necessary for our situation
         fn = secure_filename(f.filename)
         f.save(fn)
-        print(app.root_path)
+        # returning the file, works the same with send_from_directory
         return send_file('test.xlsx', as_attachment=True)
         # return 'worked !'
-    if request.method == 'GET':
-        f = request.files['file']
-        fn = secure_filename(f.filename)
-        return send_file(fn, as_attachment=True)
 
 
 if __name__ == '__main__':
